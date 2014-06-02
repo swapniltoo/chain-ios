c56a7d1be73e1cd7dbf806e85db2f92167ed1052
Matt Matteson <matt@mattmatteson.com>
2014-06-02 16:48:09 -0700

    Add basic auth.

--- Chain/Chain.m
+++ Chain/Chain.m
@@ -9,8 +9,16 @@
 @implementation Chain
 
 + (void)getBalance {
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURL *url = [NSURL URLWithString:@"https://GUEST-TOKEN@api.chain.com/v1/bitcoin/addresses/17x23dNjXJLzGMev6R63uyRhMWP1VHawKc"];
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSString *APIToken = @"GUEST-TOKEN";
+    NSData *APITokenData = [APIToken dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *base64EncodedCredential = [APITokenData base64EncodedStringWithOptions:0];
+    NSString *authString = [NSString stringWithFormat:@"Basic %@", base64EncodedCredential];
+    sessionConfiguration.HTTPAdditionalHeaders = @{@"Accept": @"application/json",
+      @"Accept-Language": @"en",
+      @"Authorization": authString};
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+    NSURL *url = [NSURL URLWithString:@"https://api.chain.com/v1/bitcoin/addresses/17x23dNjXJLzGMev6R63uyRhMWP1VHawKc"];
     [[session dataTaskWithURL:url
             completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
                 NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
