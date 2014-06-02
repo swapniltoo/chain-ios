b238e3fb22ea271d51e190870ff7d552cb4dbdcf
Matt Matteson <matt@mattmatteson.com>
2014-06-02 16:23:57 -0700

    Chain.h/m

---
+++ Chain/Chain.h
@@ -0,0 +1,11 @@
+//
+//  Chain.h
+//
+//  Copyright (c) 2014 Chain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Chain : NSObject
+
+@end
---
+++ Chain/Chain.m
@@ -0,0 +1,12 @@
+//
+//  Chain.m
+//
+//  Copyright (c) 2014 Chain. All rights reserved.
+//
+
+#import "Chain.h"
+
+@implementation Chain
+
+
+@end
