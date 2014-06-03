9fa89f86fd6f2ff4c9b1572877185bcae8c3bf5c
Ryan Smith <r@32k.io>
2014-06-03 15:00:28 -0700

    Log in lib examples

--- Example/Hello Chain/Hello Chain/CNAppDelegate.m
+++ Example/Hello Chain/Hello Chain/CNAppDelegate.m
@@ -14,11 +14,11 @@
     Chain *chainInstance = [Chain sharedInstanceWithToken:@"GUEST-TOKEN"];
     
     [chainInstance getAddress:@"1A3tnautz38PZL15YWfxTeh8MtuMDhEPVB" completionHandler:^(NSDictionary *dictionary, NSError *error) {
-        NSLog(@"%@", dictionary);
+        NSLog(@"data=%@ error=%@", dictionary, error);
     }];
     
     [chainInstance sendTransaction:@"" completionHandler:^(NSDictionary *dictionary, NSError *error) {
-        
+        NSLog(@"data=%@ error=%@", dictionary, error);
     }];
     
     return YES;
